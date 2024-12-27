6492c42cf95ecff0a6ba9f0dd655a9c071c22805
rami <rami@DESKTOP-LV21IQC>
2024-12-27 18:32:23 +0300

    minuts 26:13

--- Program.cs
+++ Program.cs
@@ -5,39 +5,39 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.Configure<RouteOptions>(options =>
 {
-    options.ConstraintMap.Add("countryName",typeof(CountryRouteConstraint));
+    options.ConstraintMap.Add("countryName", typeof(CountryRouteConstraint));
 });
 
 
 var app = builder.Build();
 
-//app.UseMiddleware<Population>();
-//app.UseMiddleware<Capital>();
-app.UseRouting();
-app.UseEndpoints(endpoints =>
+app.Use(async (context, next) =>
 {
-    //endpoints.MapGet("{first:int}/{second:bool}/{*ETC..}", async context =>
-    //{
-    //    await context.Response.WriteAsync("Request was routed\n");
-    //    foreach (var kvp in context.Request.RouteValues)
-    //    {
-    //        await context.Response.WriteAsync($"{kvp.Key} : {kvp.Value}\n");
-    //    }
-    endpoints.MapGet("{capital}/{country:countryName}", Capital.EndPoint);
-
-    //});
-    //endpoints.MapGet("capital/{country=syria}", Capital.EndPoint);
-    endpoints.MapGet("capital/{country:regex(^syria|lebanon|damascus$)}", Capital.EndPoint);
-    endpoints.MapGet("size/{city?}", Population.EndPoint)
-    .WithMetadata(
-        new RouteNameMetadata("population")
-        );
-});
-app.Run(async (context) =>
-{
-    await context.Response.WriteAsync("Terminal Middleware reached");
+    Endpoint? end = context.GetEndpoint();
+    if (end != null)
+    {
+        await context.Response.WriteAsync($"{end.DisplayName} Selected \n");
+    }
+    else
+    {
+        await context.Response.WriteAsync("No endpoint selected");
+    }
+    await next();
 });
 
+
+app.Map("{number:int}", async context =>
+{
+    await context.Response.WriteAsync("Routing to int endpoint");
+}).Add(x =>
+{
+    ((RouteEndpointBuilder)x).Order = 0;
+    x.DisplayName = "Int EndPoint";  
+    });
+app.Map("{number:double}", async context =>
+{
+    await context.Response.WriteAsync("Routing to double endpoint");
+}).WithDisplayName("Double Endpoint").Add(x => ((RouteEndpointBuilder)x).Order = 1);
 app.MapFallback(async context =>
 {
     await context.Response.WriteAsync("Routed to fallback endpoint");
