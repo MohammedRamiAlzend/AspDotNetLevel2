dc27964c40a8ce72868a2dffbbf631e32962bcfe
MohammedRamiAlzend <162128732+MohammedRamiAlzend@users.noreply.github.com>
2025-01-13 17:41:15 +0300

    End of session 14

---
+++ ConsentMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http.Features;
+
+namespace Platform;
+
+public class ConsentMiddleware
+{
+    private RequestDelegate next;
+    public ConsentMiddleware(RequestDelegate next)
+    {
+        this.next = next;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        if(context.Request.Path == "/consent")
+        {
+            ITrackingConsentFeature? consentFeature = context.Features.Get<ITrackingConsentFeature>();
+            if(consentFeature is not null)
+            {
+                if(consentFeature.HasConsent is false)
+                {
+                    consentFeature.GrantConsent();
+                }
+                else
+                {
+                    consentFeature.WithdrawConsent();
+                }
+                await context.Response.WriteAsync(consentFeature.HasConsent ? "Consent Granted\n" : "consent withdraw\n");
+            }
+            else
+            {
+                await next(context);
+            }
+        
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,27 +2,53 @@ using Platform;
 using Microsoft.AspNetCore.HttpLogging;
 using Microsoft.Extensions.FileProviders;
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddHttpLogging(options =>
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddSession(opts =>
 {
-    options.LoggingFields = HttpLoggingFields.RequestMethod |
-    HttpLoggingFields.RequestPath|
-    HttpLoggingFields.ResponseStatusCode
-    ;
-}); 
-
-
-var app = builder.Build();
-
-app.UseHttpLogging();
-app.UseStaticFiles();
-var env = app.Environment;
-app.UseStaticFiles(new StaticFileOptions
+    opts.IdleTimeout = TimeSpan.FromMinutes(30);
+    opts.Cookie.IsEssential = true;
+});
+/*
+builder.Services.Configure<CookiePolicyOptions>(opts =>
 {
-    FileProvider = new PhysicalFileProvider($"{env.ContentRootPath}/staticfiles"),
-    RequestPath = "/files"
+    opts.CheckConsentNeeded = context => true;
 });
 
-app.MapGet("population/{city?}", Population.EndPoint);
+ */
+var app = builder.Build();
+app.UseSession();
+app.UseMiddleware<ConsentMiddleware>();
+/*
+app.UseCookiePolicy(); 
+app.UseMiddleware<ConsentMiddleware>();
 
 
+app.MapGet("/cookie", async context =>
+{
+    int counter1 = int.Parse(context.Request.Cookies["counter1"] ?? "0") + 1;
+    context.Response.Cookies.Append("counter1", counter1.ToString(),
+        new CookieOptions
+        {
+            MaxAge = TimeSpan.FromMinutes(30),
+            IsEssential = true,
+            
+        });
+    int counter2 = int.Parse(context.Request.Cookies["counter2"] ?? "0") + 1;
+    context.Response.Cookies.Append("counter2", counter1.ToString(),
+    new CookieOptions
+    {
+        MaxAge = TimeSpan.FromMinutes(30)
+    });
+    await context.Response.WriteAsync($"counter1:{counter1} , Counter2:{counter2}");
+});
+app.MapGet("clear", context =>
+{
+    context.Response.Cookies.Delete("counter1");
+    context.Response.Cookies.Delete("counter2");
+    context.Response.Redirect("/");
+    return Task.CompletedTask;
+});
+ */
+app.MapFallback(async context => await context.Response.WriteAsync("Hello World!"));
 app.Run();
