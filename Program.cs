f62e990164b54680f6f51b1d5f8883f9a52c7207
MohammedRamiAlzend <162128732+MohammedRamiAlzend@users.noreply.github.com>
2025-01-13 17:58:48 +0300

    fix ConsentMiddleware white screan

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 using Platform;
-using Microsoft.AspNetCore.HttpLogging;
-using Microsoft.Extensions.FileProviders;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDistributedMemoryCache();
 
@@ -9,46 +8,20 @@ builder.Services.AddSession(opts =>
     opts.IdleTimeout = TimeSpan.FromMinutes(30);
     opts.Cookie.IsEssential = true;
 });
-/*
-builder.Services.Configure<CookiePolicyOptions>(opts =>
-{
-    opts.CheckConsentNeeded = context => true;
-});
 
- */
 var app = builder.Build();
 app.UseSession();
 app.UseMiddleware<ConsentMiddleware>();
-/*
-app.UseCookiePolicy(); 
-app.UseMiddleware<ConsentMiddleware>();
 
 
-app.MapGet("/cookie", async context =>
+app.MapGet("/session", async context =>
 {
-    int counter1 = int.Parse(context.Request.Cookies["counter1"] ?? "0") + 1;
-    context.Response.Cookies.Append("counter1", counter1.ToString(),
-        new CookieOptions
-        {
-            MaxAge = TimeSpan.FromMinutes(30),
-            IsEssential = true,
-            
-        });
-    int counter2 = int.Parse(context.Request.Cookies["counter2"] ?? "0") + 1;
-    context.Response.Cookies.Append("counter2", counter1.ToString(),
-    new CookieOptions
-    {
-        MaxAge = TimeSpan.FromMinutes(30)
-    });
-    await context.Response.WriteAsync($"counter1:{counter1} , Counter2:{counter2}");
+    int counter1 = (context.Session.GetInt32("counter1") ?? 0) + 1;
+    int counter2 = (context.Session.GetInt32("counter2") ?? 0) + 1;
+    context.Session.SetInt32("counter1", counter1);
+    context.Session.SetInt32("counter2", counter2);
+    await context.Session.CommitAsync();
+    await context.Response.WriteAsync($"Counter:{counter1}, Counter2:{counter2}");
 });
-app.MapGet("clear", context =>
-{
-    context.Response.Cookies.Delete("counter1");
-    context.Response.Cookies.Delete("counter2");
-    context.Response.Redirect("/");
-    return Task.CompletedTask;
-});
- */
-app.MapFallback(async context => await context.Response.WriteAsync("Hello World!"));
+//app.MapFallback(async context => await context.Response.WriteAsync("Hello World!"));
 app.Run();
